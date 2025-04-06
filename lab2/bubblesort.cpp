40d712b903ff2501770c963fbf1728b6efd8e0db
Anxhela B <bobocianxhela@gmail.com>
2025-04-06 16:09:06 +0200

    Add files via upload

---
+++ lab2/bubblesort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array ne fillim: ";
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
+
+    cout << "Array pas bubble sort: ";
+    printArray(arr, size);
+
+    return 0;
+}
---
+++ lab2/selectionsort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array ne fillim ";
+    printArray(arr, size);
+
+    selectionSort(arr, size);
+
+    cout << "Array me selection sort: ";
+    printArray(arr, size);
+
+    return 0;
+}
